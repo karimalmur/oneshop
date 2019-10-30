6b41b063acbce8985e143d6a0e552e76f7f9e7ba
karim <karimit@live.com>
2019-10-30 06:04:16 +0200

    Adds support for Hot reload typography in development

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -24,6 +24,12 @@ fairyGateTheme.overrideThemeStyles = () => ({
 })
 
 const typography = new Typography(fairyGateTheme)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles();
+}
+
 export const { scale, rhythm, options } = typography
 
 export default typography
